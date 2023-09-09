788250336f4452957e004ae13ad9c0f2db716f38
Vlas <dron.2001sv@mail.ru>
2023-09-09 14:15:05 +0300

    <refactor><Изменить ErrorResponse>

--- core/src/main/java/ru/practicum/exception/error/ErrorResponse.java
+++ core/src/main/java/ru/practicum/exception/error/ErrorResponse.java
@@ -1,14 +1,19 @@
 package ru.practicum.exception.error;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
-    String error;
-
-    public ErrorResponse(String error) {
-        this.error = error;
-    }
-
-    // геттеры необходимы, чтобы Spring Boot мог получить значения полей
-    public String getError() {
-        return error;
-    }
+    private String status;
+    private String reason;
+    private String message;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
 }
--- core/src/main/java/ru/practicum/exception/handler/ErrorHandler.java
+++ core/src/main/java/ru/practicum/exception/handler/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.exception.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,6 +11,8 @@ import ru.practicum.exception.ResourceNotFoundException;
 import ru.practicum.exception.ValidationException;
 import ru.practicum.exception.error.ErrorResponse;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
@@ -17,21 +20,25 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse expNotFound(final ResourceNotFoundException exception) {
-        log.error("ERROR 404:{}", exception.getMessage());
-        return new ErrorResponse(exception.getMessage());
+        log.error("ERROR 404: {}", exception.getMessage());
+        return new ErrorResponse(HttpStatus.NOT_FOUND.getReasonPhrase(), "The required object was not found.",
+                exception.getMessage(), LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse expValidation(final ValidationException exception) {
         log.error("ERROR 400: {}", exception.getMessage());
-        return new ErrorResponse(exception.getMessage());
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.getReasonPhrase(), "Incorrectly made request.",
+                exception.getMessage(), LocalDateTime.now());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler({ConflictException.class, DataIntegrityViolationException.class})
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse expConflict(final ConflictException exception) {
+    public ErrorResponse expConflict(final RuntimeException exception) {
         log.error("ERROR 409: {}", exception.getMessage());
-        return new ErrorResponse(exception.getMessage());
+        return new ErrorResponse(HttpStatus.CONFLICT.getReasonPhrase(),
+                "For the requested operation the conditions are not met.",
+                exception.getMessage(), LocalDateTime.now());
     }
 }
